13b05f36b2407c66422ebefb7a3b1203bf296e3e
Emith Dinsara <emithdinsara03@gmail.com>
2023-02-23 20:23:23 +0530

    Updated dataset variables used in the code
    Instead of using separate datasets for testing and training, split training dataset into 80% training and 20% testing. Yet accuracy <30% :(

--- BackEnd/Code/personalityTestKNN.py
+++ BackEnd/Code/personalityTestKNN.py
@@ -4,9 +4,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 
 modelKNN = KNeighborsClassifier()
-modelKNN.fit(datasetClass.trainingX, datasetClass.trainingy)
+modelKNN.fit(datasetClass.X_train_split, datasetClass.y_train_split)
 
-predictionKNN = modelKNN.predict(datasetClass.testingX)
+predictionKNN = modelKNN.predict(datasetClass.X_test_split)
 
-score = accuracy_score(datasetClass.testingy, predictionKNN)
+score = accuracy_score(datasetClass.y_test_split, predictionKNN)
 print(score)
