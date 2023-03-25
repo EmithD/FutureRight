fe1ee23e2d1a3ff9d2372e7d1ee2a06cfc07df1b
Alfa Azmi <125366838+Alfa-Azmi@users.noreply.github.com>
2023-03-26 01:45:00 +0530

    data pre processing
    data pre processing

--- BackEnd/Code/Career recommendation/careerRecommendation.py
+++ BackEnd/Code/Career recommendation/careerRecommendation.py
@@ -1,2 +1,19 @@
 import pandas as pd 
 
+data=pd.read_csv("BackEnd\\Data\\CareerPredictions\\TrainSet.csv")
+# make a copy of the original dataframe
+edited2 = data.copy()
+# replacing values in 'stream' column
+edited2['Stream'].replace(['arts','commerce ', 'physical science', 'biological science', 'engineering technology',
+                          'biosystems technology'], [0, 1, 2, 3, 4, 5], inplace=True)
+
+#replacing values in the 'personality' column
+edited2['Personality'].replace(['dependable ','serious','lively','responsible','extraverted'],
+                                [0,1,2,3,4], inplace=True )
+
+#replacing values in the 'Academic Performance' column
+edited2['Academic Performance'].replace(['Excellent','Good','Average','Below Average'],
+                                [0,1,2,3], inplace=True )
+
+# saving the edited dataframe
+edited2.to_csv("BackEnd\\Data\\CareerPredictions\\CareersEdited.csv")
