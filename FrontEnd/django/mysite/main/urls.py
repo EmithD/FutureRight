30a0920506a296f0ca3de587774b8d2149775cfe
dulanvidanagamage <dulan.vidz@gmail.com>
2023-03-27 00:49:26 +0530

    about us added

--- FrontEnd/django/mysite/main/urls.py
+++ FrontEnd/django/mysite/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("home/", views.index, name="index"),
+    path("about_us/", views.about_us, name="about_us"),
 ]
