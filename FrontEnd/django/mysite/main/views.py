414e2acc7f9bb4ab597dbae1c2d5eb4a19800c89
dulanvidanagamage <dulan.vidz@gmail.com>
2023-03-27 02:01:53 +0530

    help , about us ,start test and  review submitted

--- FrontEnd/django/mysite/main/views.py
+++ FrontEnd/django/mysite/main/views.py
@@ -7,5 +7,11 @@ def index(response):
     return render(response, "main/base.html", {})
 def about_us(response):
     return render(response, "main/about_us.html", {})
+def help(response):
+    return render(response, "main/help.html", {})
+def start_test(response):
+    return render(response, "main/start_test.html", {})
+def review_test(response):
+    return render(response, "main/review_test.html", {})
 
 
