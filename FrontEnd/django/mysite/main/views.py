8bfa120226508410622bdbf218711db6941e71d6
Emith Dinsara <88488807+EmithD@users.noreply.github.com>
2023-04-05 06:31:30 +0530

    Major tweaks done
    Final career recommendation successful and working

--- FrontEnd/django/mysite/main/views.py
+++ FrontEnd/django/mysite/main/views.py
@@ -3,8 +3,39 @@ from django.views.decorators.csrf import csrf_exempt
 import joblib
 import os
 from django.conf import settings
+import csv
 
 # Create your views here.
+
+finalPersonality = []
+streamArray = []
+
+
+def careerRecommendation(stream, personality, academic_performance):
+
+    theStreams = {
+        0: 'arts',
+        1: 'commerce',
+        2: 'physical science',
+        3: 'biological science',
+        4: 'engineering technology',
+        5: 'BIOSYSTEMS TECHNOLOGY'
+    }
+
+    finallyStream = theStreams[stream]
+
+    careers = []
+    path = os.path.join(settings.BASE_DIR, 'mysite\\TrainSet.csv')
+    with open(path) as csvfile:
+        csvreader = csv.DictReader(csvfile)
+
+        # loop through rows
+        for row in csvreader:
+            if row['Stream'] == finallyStream:
+                if row['Personality'] == personality:
+                    if row['Academic Performance'] == academic_performance:
+                        careers.append(row['Career'])
+    return careers
 
 
 def index(response):
@@ -20,11 +51,40 @@ def help(response):
 
 
 def start_test(response):
-    return render(response, "main/start_test.html", {})
+    return render(response, "main/Start_Test.html", {})
 
 
+@csrf_exempt
 def review_test(response):
-    return render(response, "main/review_test.html", {})
+
+    if response.method == 'POST':
+
+        stream = int(response.POST['stream'])
+        streamArray.clear()
+        streamArray.append(stream)
+        sub_1 = int(response.POST['subject1'])
+        sub_1_r = int(response.POST['sub1_result'])
+        sub_2 = int(response.POST['subject2'])
+        sub_2_r = int(response.POST['sub2_result'])
+        sub_3 = int(response.POST['subject3'])
+        sub_3_r = int(response.POST['sub3_result'])
+        gen_eng = int(response.POST['gen_english'])
+
+        academicValues = [
+            [stream, sub_1, sub_1_r, sub_2, sub_2_r, sub_3, sub_3_r, gen_eng]]
+
+        path = os.path.join(settings.BASE_DIR,
+                            'mysite\\academicPerformance.joblib')
+        model = joblib.load(path)
+
+        finalPred = model.predict(academicValues)[0]
+
+        print(stream, finalPersonality[0], finalPred)
+
+        context = {'academics': finalPred,
+                   'personality': finalPersonality[0], 'careers': careerRecommendation(stream, finalPersonality[0], finalPred)}
+
+    return render(response, "main/Review_Test.html", context)
 
 
 def therm_use(response):
@@ -57,12 +117,8 @@ def test(response):
                             'mysite\\PersonalityTestLogisticRegression.joblib')
         model = joblib.load(path)
 
-        finalPred = model.predict(personality_values_1)[0]
+        finalPredPersonality = model.predict(personality_values_1)[0]
+        finalPersonality.clear()
+        finalPersonality.append(finalPredPersonality)
 
-    return render(response, "main/Review_Test.html", {'personality': finalPred})
-
-
-# @csrf_exempt
-# def academicTest(repsonse):
-
-#     if repsonse.method == 'POST':
+    return render(response, "main/acedemic_test.html", {})
